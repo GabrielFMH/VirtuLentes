7c9bb53d16beaa1612ed4844aa6b3d8ba9506c14
GabrielFMH <84587604+GabrielFMH@users.noreply.github.com>
2024-05-21 15:38:38 -0500

    commit 21/5/2024

---
+++ conexion/database.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct() {
+        try {
+            $this->pdo = new PDO('mysql:host=localhost;dbname=virtulentes', 'root', 'Upt2024'); 
+            //cambien la contrasena o puerto si no les funciona
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Database connection failed: ' . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
+?>
